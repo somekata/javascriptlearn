22690c8f3b73e33cc2a22908eb0161f1a2eef08e
SOMEKATA Shiro <88067020+somekata@users.noreply.github.com>
2024-12-25 11:00:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    // メニュー要素を取得
+    const headerElement = document.getElementById("headerDefault");
+  
+    // ヘッダーの内容をHTML文字列として定義
+    const headerContent = `
+        <nav id="navigation">
+    <ul>
+        <li><a href="list.html">目次</a></li>
+        <li><a href="codeSample.html">コード例</a></li>
+        <li><a href="tagTransform.html">エスケープ変換</a></li>
+    </ul>
+    </nav>     
+    `;
+  
+    // メニュー要素に内容を挿入
+    if (headerElement) {
+      headerElement.innerHTML = headerContent;
+    }
+  });
+ 
+document.addEventListener("DOMContentLoaded", () => {
+// メニュー要素を取得
+const footerElement = document.getElementById("footerDefault");
+
+// ヘッダーの内容をHTML文字列として定義
+const footerContent = `
+        <p id="footerContent">&copy; 2024 JavaScript解説</p>
+`;
+
+// メニュー要素に内容を挿入
+if (footerElement) {
+    footerElement.innerHTML = footerContent;
+}
+});
+    
